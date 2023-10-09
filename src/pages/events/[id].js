8ac1201ea4ab3055b6f8502de21ba6dc1641a4db
danny samuel <melodyslsbll90@gmail.com>
2023-10-09 13:35:43 +0700

    fix bugs

--- src/pages/events/[id].js
+++ src/pages/events/[id].js
@@ -5,64 +5,47 @@ import MetaHead from "@/components/MetaHead";
 import Link from "next/link";
 
 const ProductDetail = ({ product }) => {
-  if (!product) {
-    return (
-      <div className="py-20">
-        <Container>
-          <Col className="py-20">
-            <Row className="d-flex align-items-center">
-              <h1 className="mx-auto text-2xl">Product tidak ditemukan</h1>
-            </Row>
-            <Row className="pt-5 pb-20">
-              <Link
-                href="/events"
-                className="px-10 py-2 mx-auto text-white rounded bg-slate-500 hover:bg-slate-700"
-              >
-                Kembali
-              </Link>
-            </Row>
-          </Col>
-        </Container>
-      </div>
-    );
-  }
-
   return (
     <>
       <MetaHead title="Detail Product | smarteye.id" />
       <div className="py-20">
         <Container>
+          <Row className="pt-10">
+            <Col>
+              <h1 className="font-bold text-center text-2xl">{product.title}</h1>
+            </Col>
+          </Row>
           <Row>
             <Col>
               <div className="pt-10 oculus">
                 <iframe
                   src={product.url}
-                  title={product.title}
+                  title="products"
                   allowFullScreen
                   className="rounded-lg h-[320px] sm:h-[560px] w-full aspect-w-16 aspect-h-9 mx-auto"
                 />
               </div>
             </Col>
           </Row>
-          <Row className="py-5">
-            <Col>
-              <h1 className="text-lg sm:text-2xl font-bold">{product.title}</h1>
-            </Col>
-            <Col className="sm:w-1/2 xs:w-full xs:flex xs:justify-center xs:mt-5">
-              <div className="text-center sm:text-right titel">
+          <Row className="pt-5 pb-20">
+            <Col className="flex flex-col items-center">
+              <p className="text-justify">
+                {product.description}
+              </p>
+              <div className="flex flex-col gap-5 py-5 mx-auto tutor sm:flex-row md:flex">
+              <Link
+                  href="/events"
+                  className="px-10 py-2 mx-auto text-black rounded sm:px-20 md:px-40 bg-slate-200 hover:bg-slate-400"
+                >
+                  Back
+                </Link>
                 <Link
                   href="/events/tutorial"
-                  className="px-6 sm:px-10 py-2 mt-5 ml-5 mr-5 text-white rounded-lg bg-slate-500 hover:bg-slate-700 butuna"
+                  className="px-10 py-2 mx-auto text-white rounded sm:px-20 md:px-40 bg-slate-500 hover:bg-slate-700"
                 >
-                  Click me
+                  Next
                 </Link>
               </div>
-            </Col>
-          </Row>
-
-          <Row className="pt-5 pb-20">
-            <Col>
-              <h1 className="text-justify">{product.description}</h1>
             </Col>
           </Row>
         </Container>
